ac961c468d37cdc2ef42049b79948afc7bdf4ef7
obaidhsn <52861715+obaidhsn@users.noreply.github.com>
2023-09-13 23:04:36 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,59 +1,23 @@
-# Load model directly
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
-import torch
+from predict_sentiment import SentimentPredictor
+from translate import SentenceTranslator
+from flask import Flask, render_template, request
 
+app = Flask(__name__)
+translator = SentenceTranslator()
+sentiment_predictor = SentimentPredictor()
 
-tokenizer = AutoTokenizer.from_pretrained("nlptown/bert-base-multilingual-uncased-sentiment")
-model = AutoModelForSequenceClassification.from_pretrained("nlptown/bert-base-multilingual-uncased-sentiment")
+@app.route("/", methods=["GET", "POST"])
+def index():
+    result = ""
+    if request.method == "POST":
+        input_sentence = request.form["sentence"]
+        translated_sentence = translator.detect_and_translate(input_sentence)
+        if isinstance(translated_sentence, tuple):
+            result = translated_sentence[-1]
+        else:
+            result = sentiment_predictor.get_sentiment(translated_sentence)
+            result = "Predicted Sentiment: " + result
+    return render_template("index.html", result=result)
 
-sentence = "it is a good video"
-
-from googletrans import Translator, LANGUAGES
-
-def detect_and_translate(text):
-    # Initialize the translator
-    translator = Translator()
-
-    # Detect the language of the text
-    detected_language = translator.detect(text).lang
-
-    # Translate the text to English
-    if detected_language != 'en':
-        translation = translator.translate(text, src=detected_language, dest='en').text
-    else:
-        translation = text  # Text is already in English
-
-    return detected_language, translation
-
-try:
-    detected_language, sentence = detect_and_translate(sentence)
-except:
-    detected_language = None 
-
-print(f"Detected Language: {LANGUAGES.get(detected_language)}")
-print(f"Translated Text: {sentence}")
-
-
-inputs = tokenizer(sentence, return_tensors="pt", padding=True, truncation=True)
-with torch.no_grad():
-    output = model(**inputs)
-
-# Extract the logits tensor
-logits = output.logits
-
-# If you want the predicted class labels, you can use argmax
-predicted_class_index = logits.argmax().item()
-
-# If you want the probability scores for each class, you can apply softmax
-softmax_scores = torch.softmax(logits, dim=1)
-predicted_probabilities = softmax_scores[0].tolist()  # Assuming a batch size of 1
-
-# If you have access to class labels, you can map the predicted index to a label
-class_labels = ["Very Negative", "Negative", "Neutral", "Positive", "Very Positive"]
-predicted_class_label = class_labels[predicted_class_index]
-
-# Print the results
-print("Predicted Class Label:", predicted_class_label)
-
-
-
+if __name__ == "__main__":
+    app.run(debug=True)
